ba7ef387ca2f97907e57302ad2e678edc10b9e2c
andersonlenzjava <105016572+andersonlenzjava@users.noreply.github.com>
2023-04-14 04:45:53 -0300

    first steps course rest erudio

--- src/main/java/br/com/erudio/springjavaerudio/Startup.java
+++ src/main/java/br/com/erudio/springjavaerudio/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJavaErudioApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJavaErudioApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
