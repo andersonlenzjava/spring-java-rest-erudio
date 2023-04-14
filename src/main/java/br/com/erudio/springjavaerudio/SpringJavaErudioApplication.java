f9d03ed63b22054feb74ef29c80d2131b9038e0c
andersonlenzjava <105016572+andersonlenzjava@users.noreply.github.com>
2023-04-13 21:28:34 -0300

    first commit

---
+++ src/main/java/br/com/erudio/springjavaerudio/SpringJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.springjavaerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaErudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/erudio/springjavaerudio/SpringJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.springjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
